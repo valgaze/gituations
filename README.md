c3c74c0c77f01cc6a19b7229376d141be1d529c6
Victor Algaze <valgaze@gmail.com>
2017-07-21 20:10:00 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# gituations
+Exercise repo with some sticky gituations
+
+Companion blog post: https://medium.com/@valgaze/utility-post-how-to-escape-3-tricky-gituations-c3ea88908239
+
+
+**Gituation, (noun):** A situation, oftentimes unpleasant or confusing, involving git commit histories
+
+This repo contains three branches, each with a different troublesome gituation. If you reach an unrecoverable state, you can reset the repo's branches by running ```npm run rebuild```
+
+
+Branches & clues:
+
+| Branch  |  Issue + clue |
+|---|:-:|
+| ex/1  | Typo in first commit (amend the message)  |
+| ex/2  | Too many commits (squash the commits together)  |
+| ex/3  | Most recent commit broke something (revert back)  |
---
+++ scripts/rebuild.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+## Presume running from scripts/
+cd ..
+
+root=$PWD
+
+function gl() {
+  git log
+}
+
+function gaa() {
+  git add . && git commit -m "$@"
+}
+function gco() {
+  git checkout $@
+}
+
+function killBranch() {
+  git branch -D $1
+}
+
+function amplify() {
+  echo "################################################"
+  echo "#"
+  echo "#"
+  echo "#  $@"
+  echo "#"
+  echo "#"
+  echo "################################################"
+}
+
+## Clear existing
+killBranch ex/1
+killBranch ex/2
+killBranch ex/3
+
+amplify "Generating ex1..."
+gco master && gco -b ex/1
+
+cat > $root/ex1.txt <<EOL
+
+A bunch of wonderful example 1 stuff
+
+EOL
+gaa "Ex1, fix thizzz typo!"
+
+
+amplify "Generating ex2..."
+gco master && gco -b ex/2
+touch $root/ex2.txt
+array=( a b c d e)
+target=c
+for i in "${array[@]}"
+do
+echo $i >>$root/ex2.txt
+if [ "$i" == "$target" ]; then
+  gaa "Added $i, there is something important on this commit"
+else
+  gaa "Added $i"
+fi
+done
+echo "Ex2, squash commits up to a and replace the final commit message with 'Ex2, Added a bunch of letters'" >> $root/ex2.txt
+gaa "Ex2, squash those commits"
+
+amplify "Generating ex3..."
+gco master && gco -b ex/3
+
+cat > $root/ex3.js <<EOL
+
+let num = 1;
+
+if (num === 2) {
+  console.log('Fires correctly?');
+}
+
+EOL
+gaa "Ex3, initial scaffolding"
+
+
+cat > $root/ex3.js <<EOL
+
+let num = 1;
+num++;
+if (num === 2) {
+  console.log('Fires correctly?');
+}
+
+EOL
+gaa "Ex3, working version"
+
+cat > $root/ex3.js <<EOL
+
+let num = 1;
+num + 100;
+if (num === 2) {
+  console.log('Fires correctly?');
+}
+
+EOL
+gaa "Ex3, Something's broken..."
+
+gco master
+amplify "Operation Complete. Run $ git branch to see all branches"
+
+git branch -a
